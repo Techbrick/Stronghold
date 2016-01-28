3ef1b0a7f944b5a0a41607c51441cf881adc6a7c
Noah Z <FRCNoahZ@users.noreply.github.com>
2016-01-28 11:18:24 -0500

    Update Position.cpp

--- Code-v1/Position.cpp
+++ Code-v1/Position.cpp
@@ -1,39 +1,70 @@
-
 /*
- * Position.h
+ * Position.cpp
  *
- *  Created on: Jan 26, 2016
- *      Author: Owner
+ *  Created on: Jan 25, 2016
+ *      Author: Noah Zbozny
  */
-
-#ifndef SRC_POSITION_H_
-#define SRC_POSITION_H_
-
+//origin is nearest left corner
 #include "WPILib.h"
+#include "Constants.cpp"
 #include "pthread.h"
-#include "AHRS.h"
+#include "math.h"
+#include "Position.h"
 
-class Position {
-	AHRS gyro; //navx mxp
-	BuiltInAccelerometer accel;
-	Timer xTimer;
-	Timer yTimer;
-	float xAcceleration;
-	float yAcceleration;
-	float xDistance;
-	float yDistance;
-	//static float xPos = Constants::xStartPos; //doesn't like static without constexpr
-	//static float yPos = Constants::yStartPos;
-	float xPos = Constants::xStartPos;
-	float yPos = Constants::yStartPos;
-
-public :
-	Position();
-	void Setup();
-	float trackX();
-	float trackY();
-	float angleToTower();
-};
+#define PI 3.14159265
 
 
-#endif /* SRC_POSITION_H_ */
+	//TODO: Add in absolute locations of obstacles so that the robot can move to them && add in vision capabilities
+	//TODO: Set to update every ms - Kyle
+
+	Position::Position():
+	gyro(I2C::Port::kMXP), //assuming we're on this port
+	accel()
+	{
+		xAcceleration = 0;
+		yAcceleration = 0;
+		xDistance = 0;
+		yDistance = 0;
+	}
+
+	void Position::Setup() {
+		xTimer.Start();
+		xTimer.Reset();
+		yTimer.Start();
+		yTimer.Reset();
+		gyro.Reset();
+	}
+	float Position::trackX() {
+		xAcceleration = accel.GetX() * cos((gyro.GetAngle() - 90) * PI / 180); //angle + 90? Will have to test
+		xDistance = .5 * xAcceleration * xTimer.Get() * xTimer.Get();
+		xPos = xPos + xDistance;
+		xTimer.Reset();
+		return xPos;
+	}
+
+	float Position::trackY() {
+		yAcceleration = accel.GetY() * sin((gyro.GetAngle() - 90) * PI / 180); //again, we'll have to play with it
+		yDistance = .5 * yAcceleration * yTimer.Get() * yTimer.Get();
+		yPos = yPos + yDistance;
+		yTimer.Reset();
+		return yPos;
+	}
+
+	float Position::angleToTower() {
+		float theta = gyro.GetAngle();
+		float xToTower = Constants::towerX - xPos;
+		float yToTower = Constants::towerY - yPos;
+		float dotProduct;
+		float uLength;
+		float vLength;
+		float angleToTower;
+
+		dotProduct = (-1 * xPos) * (xToTower) + (-1 * xPos * tan(90 - theta)) * (yToTower);
+		uLength = sqrt(pow(-1 * xPos, 2) + pow(-1 * xPos * tan(90 - theta), 2));
+		vLength = sqrt(pow(xToTower, 2) + pow(yToTower, 2));
+		angleToTower = acos(dotProduct/(uLength * vLength)); //linear algebra
+		return angleToTower;
+	}
+
+
+
