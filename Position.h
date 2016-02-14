9ba6c3776f88a0030541db279c3cef5745dcf305
Noah Z <FRCNoahZ@users.noreply.github.com>
2016-02-14 08:52:09 -0500

    Update Position.h

--- Position.h
+++ Position.h
@@ -1,52 +1,41 @@
-
 /*
  * Position.h
  *
- *  Created on: Jan 26, 2016
- *      Author: Noah Zbozny
+ *  Created on: Feb 13, 2016
+ *      Author: Owner
  */
 
 #ifndef SRC_POSITION_H_
 #define SRC_POSITION_H_
 
 #include "WPILib.h"
-#include "pthread.h"
 #include "AHRS.h"
+#include "Constants.h"
 
 class Position {
 	AHRS mxp; //navx mxp
-	Timer xAccelTimer;
-	Timer yAccelTimer;
-	Timer runOnceTimer;
-	float xAcceleration;
-	float yAcceleration;
-	float xVelocity;
-	float yVelocity;
-	float xDistance;
-	float yDistance;
-	float xPosAccel;
-	float yPosAccel;
-	float xTime;
-	float yTime;
-	float xPos = Constants::xStartPos;
-	float yPos = Constants::yStartPos;
-	int counter = 0;
-	//static float xPos = Constants::xStartPos; //doesn't like static without constexpr
-	//static float yPos = Constants::yStartPos;
+	float angle;
+	float pitch;
+	float encoderTicks;
+	float xPos;
+	float yPos;
+	float wheelRadius = Constants::wheelRadius;
+	float obstacleXPos [5] = {0, 0, 0, 0, 0};
+	float obstacleYPos [5] = {0, 0, 0, 0, 0};
 
-public :
+public:
 	Position();
-	void Setup();
-	void AccelerometerTrackX();
-	void AccelerometerTrackY();
-	void TalonTrackX();
-	void TalonTrackY();
-	void Update();
+	inline float GetAngle();
+	inline float GetPitch();
+	void TrackX(bool movingForward);
+	void TrackY(bool movingForward);
+	void Update(bool movingForward);
+	int NearestObstacle();
 	float GetX();
 	float GetY();
 	float AngleToTower();
 	float DistanceToTower();
+	void Calibrate();
 };
-
 
 #endif /* SRC_POSITION_H_ */
