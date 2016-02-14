a008370262ca7fa9e1afaf1eb6edf33a381f7284
Noah Z <FRCNoahZ@users.noreply.github.com>
2016-02-14 08:53:10 -0500

    Update Shooter.h

--- Shooter.h
+++ Shooter.h
@@ -1,4 +1,5 @@
 #include "WPILib.h"
+#include "Constants.h"
 
 #ifndef SRC_SHOOTER_H
 #define SRC_SHOOTER_H
@@ -6,14 +7,24 @@
 class Shooter {
 	CANTalon left;
 	CANTalon right;
+	CANTalon aim;
+	DigitalInput ballSensor;
+	Servo servo;
 
 public:
-	Shooter(uint32_t leftTalon, uint32_t rightTalon);
-	
+	Shooter(uint32_t leftTalon, uint32_t rightTalon, uint32_t angleTalon);
+
+	void SetSpeed(float leftSpeed, float rightSpeed);
+	void SetAngle(float angle);
 	void Enable();
 	void Disable();
-
-	void setSpeed(float leftSpeed, float rightSpeed);
+	void PrepareShooter();
+	void PrepareShooter(float angle, float speed);
+	void LoadBall();
+	void Shoot();
+	bool HasBall();
+	float WheelSpeed();
+	float Angle();
 };
 
 #endif
