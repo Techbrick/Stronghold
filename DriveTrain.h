c1c281577007825abe8bf7dc1936bc2bd89287a4
Noah Z <FRCNoahZ@users.noreply.github.com>
2016-02-14 23:12:48 -0500

    Update DriveTrain.h

--- DriveTrain.h
+++ DriveTrain.h
@@ -1,13 +1,21 @@
 #include "WPILib.h"
+#include "Position.h"
 
 #ifndef SRC_DRIVETRAIN_H
 #define SRC_DRIVETRAIN_H
 
 class DriveTrain : public RobotDrive
 {
-	CANTalon left, right;
+	CANTalon left;
+	CANTalon right;
+	Position position;
 public:
 	DriveTrain(uint32_t leftDeviceID, uint32_t rightDeviceID);
+	void Enable();
+	void Disable();
+	void TankDrive(float leftSpeed, float rightSpeed);
+	void TurnToAngle(float angle);
+	void MoveDistance(float distance, float speed);
 
 };
 
