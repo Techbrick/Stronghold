278cb60c6eacac5b0521721907e74bbf20305ca9
carlyheiner <carlyheiner@users.noreply.github.com>
2016-02-16 20:44:34 -0500

    intial commit of arm class

---
+++ Arm.cpp
@@ -0,0 +1,73 @@
+#include "Arm.h"
+
+Arm::Arm(uint32_t beltMoverID) :
+beltMover(beltMoverID)
+{
+
+}
+
+void Arm::EnableTalons ()
+{
+	/* if (!ArmMover.IsEnabled())
+		ArmMover.Enable();
+	 */
+
+	if (!beltMover.IsEnabled())
+		beltMover.Enable();
+}
+
+void Arm::DisableTalons ()
+{
+	/* if (ArmMover.IsEnabled())
+		ArmMover.Disable();
+	*/
+
+	if (beltMover.IsEnabled())
+		beltMover.Disable();
+}
+
+void Arm::MoveBelt (float speed)
+{
+	if(beltMover.IsFwdLimitSwitchClosed() && speed > 0) /*upper limit switch hit && speed > 0*/
+	{
+	beltMover.Set(0);
+	}
+	else if(beltMover.IsRevLimitSwitchClosed() && speed < 0)/*lower limit switch hit && speed < 0*/
+	{
+	beltMover.Set(0);
+	}
+	else
+	{
+		beltMover.Set(speed);
+	}
+}
+
+void Arm::PositionBelt (float position)
+{
+
+}
+
+/* void Arm::SetAngle (float angle)
+{
+
+}
+
+void Arm::OpenSallyPort ()
+{
+
+}
+
+void Arm::OpenDrawbridge ()
+{
+
+} */
+
+void Arm::OpenPortcullis ()
+{
+
+}
+
+void Arm::MoveSeesaw ()
+{
+
+}
