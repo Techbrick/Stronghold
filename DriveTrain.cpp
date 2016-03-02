b4a70b443012f08c9109a5c53876a87127d04e83
Numeri <Numeri@users.noreply.github.com>
2016-03-01 20:50:37 -0500

    Fixed the rest of the reorder errors and the uninit'd variable error

--- DriveTrain.cpp
+++ DriveTrain.cpp
@@ -2,12 +2,12 @@
 #define PI 3.14159265
 
 DriveTrain::DriveTrain(uint32_t leftMasterDeviceID, uint32_t leftSlaveDeviceID, uint32_t rightMasterDeviceID, uint32_t rightSlaveDeviceID, Position *position_):
+	RobotDrive(leftMaster, rightMaster),
 	leftMaster(leftMasterDeviceID),
 	leftSlave(leftSlaveDeviceID),
 	rightMaster(rightMasterDeviceID),
 	rightSlave(rightSlaveDeviceID),
-	position(position_),
-	RobotDrive(leftMaster, rightMaster)
+	position(position_)
 {
 	leftMaster.SetControlMode(CANTalon::ControlMode::kPercentVbus);
 	leftSlave.SetControlMode(CANTalon::ControlMode::kFollower);
