998c4a778846e093a3ec2bcedd23a008050f4d52
Numeri <Numeri@users.noreply.github.com>
2016-03-05 12:12:59 -0500

    Added debugging code

--- Robot.h
+++ Robot.h
@@ -6,18 +6,32 @@
 #include "Position.h"
 #include "Aimer.h"
 
+#include <exception>
 #include <math.h>
 #include <thread>
+#include <string>
 
 #ifndef SRC_ROBOT_H_
 #define SRC_ROBOT_H_
 
+class logger {
+	std::string msg;
+public:
+	logger(const char* msg_);
+};
+
 class Robot : public SampleRobot {
+	logger l_1;
 	DriveTrain driveTrain;
+	logger l_2;
 	Joystick driveStick;
+	logger l_3;
 	Shooter shooter;
+	logger l_4;
 	Position position;
+	logger l_5;
 	Aimer aimer;
+	logger l_6;
 public:
 	Robot();
 
